334dae4bc6912faea21bda395ee770d7c9f8a28a
PrajwalBhosale2004 <pjlbhosale@gmail.com>
2023-12-12 15:08:19 +0530

    about_us 1

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ function reveal() {
     var reveals = document.querySelectorAll(".reveal");
   
     for (var i = 0; i < reveals.length; i++) {
-      var windowHeight = window.innerHeight;
+      var windowHeight = 2*window.innerHeight;
       var elementTop = reveals[i].getBoundingClientRect().top;
       var elementVisible = 150;
   
