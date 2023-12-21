f40bbe26b81919b8c4157825689843de7d210107
Vedang <vedangkhedekar07@gmail.com>
2023-12-21 12:39:49 +0530

    feedback pages added

---
+++ php/send_feedback_in.php
@@ -0,0 +1,30 @@
+<?php
+$conn = mysqli_connect('localhost', 'u331863597_psf', 'psf@BIEC69');
+mysqli_select_db($conn, 'u331863597_feedback');
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$name = mysqli_real_escape_string($conn, $_POST['name']);
+$email = mysqli_real_escape_string($conn, $_POST['email']);
+$subject = mysqli_real_escape_string($conn, $_POST['subject']);
+$message = mysqli_real_escape_string($conn, $_POST['message']);
+
+
+$sql = "INSERT INTO investor_feedback (name, email, feedback_type, feedback) VALUES ('$name', '$email', '$subject', '$message')";
+
+if (mysqli_query($conn, $sql)) {
+    echo '<script type="text/javascript">alert("Feedback submitted.");</script>';
+        // Redirect to buttons.html
+    header("Location: investor_feedback.html");
+    exit();
+    // echo '<script type="text/javascript">window.location.href = "buttons.php";</script>';
+} else {
+    $errorMsg = "Feedback not sent: " . mysqli_error($conn);
+    echo '<script type="text/javascript">alert("' . $errorMsg . '");</script>';
+    echo '<script type="text/javascript">window.location.href = "investor_feedback.html";</script>';
+}
+
+mysqli_close($conn);
+?>
